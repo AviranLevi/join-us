b6bc7f470be829e153d618dceacae1972dee59d0
Aviran Levi <aveloofficial@gmail.com>
2020-07-14 15:16:57 +0300

    login input title minor fix

--- client/src/pages/login/login-manual/LoginManual.js
+++ client/src/pages/login/login-manual/LoginManual.js
@@ -7,7 +7,7 @@ const LoginManual = () => (
   <div className='login-manual center-items'>
     <div className='login-box'>
       <Title text='Login' classes='bold-black-text login-title' />
-      <Input title='Username' classes='username-input' />
+      <Input title='Email' classes='username-input' />
       <Input title='Password' classes='pass-input' />
       <Button text='Login' classes='bold-black-text login-btn rounded-border' />
     </div>
