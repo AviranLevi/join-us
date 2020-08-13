86fcba2f18b2f9701e033c2aeee7806f033fb61c
Aviran Levi <aveloofficial@gmail.com>
2020-08-13 16:46:20 +0300

    client - ui minor fix

--- client/src/pages/user-projects/no-projects/NoProjects.js
+++ client/src/pages/user-projects/no-projects/NoProjects.js
@@ -3,8 +3,8 @@ import Title from '../../../components/title/Title';
 
 const NoProjects = () => (
   <div className='no-projects fade-in'>
-    <Title text={`It's seems you don't have projects yet :(`} style={{ fontSize: '35px' }} />
-    <Title text={`Click the 'New page' button to create one!`} classes='bold-text' />
+    <Title text={`It's seems you don't have any projects yet :(`} style={{ fontSize: '35px' }} />
+    <Title text={`Click the 'Create New page' button to create one!`} classes='bold-text' />
   </div>
 );
 
