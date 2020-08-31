d1ed13a41c78ddfec43bf70fb5ffe0b4fff4fbc7
Aviran Levi <aveloofficial@gmail.com>
2020-08-31 15:49:57 +0300

    added origin and cradentials to server

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,7 @@ const options = {
 mongoose.connect(url, options).then(() => console.log('DB Connected'));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: env.CLIENT_ORIGIN, credentials: true }));
 app.use(sslRedirect());
 
 app.use((req, res, next) => {
