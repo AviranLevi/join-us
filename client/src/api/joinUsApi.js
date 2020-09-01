16768111c499f24d08c801d055b3a596bb57db03
Aviran Levi <aveloofficial@gmail.com>
2020-09-01 11:44:24 +0300

    client - added token to api requests

--- client/src/api/joinUsApi.js
+++ client/src/api/joinUsApi.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 import { serverURL } from '../config';
+import { accessToken } from '../../config';
+
+const api = axios.create({
+  baseURL: serverURL,
+  headers: { Authorization: `Bearer ${accessToken}` },
+});
 
 export const getUserProject = async (id) => {
   try {
@@ -14,7 +20,7 @@ export const uploadImage = async (file) => {
   try {
     const formData = new FormData();
     formData.append('file', file);
-    const { data } = await axios.post(`${serverURL}/user/image`, formData);
+    const { data } = await api.post(`/user/image`, formData);
     return data;
   } catch (error) {
     console.log(error);
@@ -23,7 +29,7 @@ export const uploadImage = async (file) => {
 
 export const removeProject = async (userId) => {
   try {
-    const { data } = await axios.delete(`${serverURL}/project/${userId}`);
+    const { data } = await api.delete(`/project/${userId}`);
     return data;
   } catch (error) {
     console.log(error);
