927e3122b395ec6f04dc535ae002d16eb31e421e
Aviran Levi <aviran@webselenese.com>
2021-08-28 21:10:50 +0300

    minor fix

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,22 +1,24 @@
-import { getTokenFromLocalStorage } from '../utils/general';
+import { getTokenFromLocalStorage } from '../utils/general'
 export const google = {
   clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
   clientSecret: process.env.REACT_APP_CLIENT_SECRET,
-};
+}
 
 export const facebook = {
   appId: process.env.REACT_APP_FACEBOOK_APP_ID,
-};
+}
 
 export const spotify = {
   clientId: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
-};
+}
 
-export const searchValues = ['spotify', 'apple', 'instagram', 'facebook', 'tiktok', 'deezer', 'youtube'];
+export const searchValues = ['spotify', 'apple', 'instagram', 'facebook', 'tiktok', 'deezer', 'youtube']
 
 export const spotifyINFO = {
   client_id: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
   client_secret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET,
-};
+}
 
-export const serverURL = process.env.REACT_APP_SERVER_URL;
+export const serverURL = process.env.REACT_APP_SERVER_URL
+
+export const DEV = process.env.DEV
--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 
 //components
-import App from './App';
-import UserLandPage from './pages/user-land-page/UserLandPage';
-import ErrorPage from './pages/error-page/ErrorPage';
+import App from './App'
+import UserLandPage from './pages/user-land-page/UserLandPage'
+import ErrorPage from './pages/error-page/ErrorPage'
 
 //stores
-import UserStore from './stores/reducers/user';
-import ProfileStore from './stores/reducers/profile';
-import FeaturesStore from './stores/reducers/features';
-import ProjectStore from './stores/reducers/project';
-import ErrorStore from './stores/reducers/errors';
+import UserStore from './stores/reducers/user'
+import ProfileStore from './stores/reducers/profile'
+import FeaturesStore from './stores/reducers/features'
+import ProjectStore from './stores/reducers/project'
+import ErrorStore from './stores/reducers/errors'
+import { DEV } from './config'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({});
-const enhancer = composeEnhancers(applyMiddleware(thunk));
+let enhancer
+if (DEV) {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+  enhancer = composeEnhancers(applyMiddleware(thunk))
+} else {
+  enhancer = applyMiddleware(thunk)
+}
 
 const reducer = combineReducers({
   user: UserStore,
@@ -26,8 +32,8 @@ const reducer = combineReducers({
   project: ProjectStore,
   errors: ErrorStore,
   profile: ProfileStore,
-});
-const store = createStore(reducer, enhancer);
+})
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
   <Provider store={store}>
@@ -42,4 +48,4 @@ ReactDOM.render(
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
-);
+)
