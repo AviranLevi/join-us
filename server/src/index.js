02890caf8cb0cc8a5cd28aa3c56175ff55bc4e7d
Aviran Levi <aviran@webselenese.com>
2021-08-28 21:33:00 +0300

    minor update

--- server/src/index.js
+++ server/src/index.js
@@ -1,41 +1,29 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import sslRedirect from 'heroku-ssl-redirect';
-import router from './router';
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import sslRedirect from 'heroku-ssl-redirect'
+import router from './router'
 
-const app = express();
-const { env } = process;
-const port = env.PORT || 5000;
-const url = env.CONNECTION_STRING || 'mongodb://localhost:27017/join-us';
+const app = express()
+const { env } = process
+const port = env.PORT || 5000
+const url = env.CONNECTION_STRING || 'mongodb://localhost:27017/join-us'
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
-};
+}
 
-mongoose.connect(url, options).then(() => console.log('DB Connected'));
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors());
-app.use(sslRedirect());
+mongoose.connect(url, options).then(() => console.log('DB Connected'))
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use(sslRedirect())
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type,Authorization, Access-Control-Request-Method, Access-Control-Request-Headers'
-  );
-  next();
-});
-
-app.use('/api', router);
+app.use('/api', router)
 
 app.listen(port, () => {
-  console.log(`app is listening to port ${port}`);
-});
+  console.log(`app is listening to port ${port}`)
+})
